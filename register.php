dea9e90c132956b26eeace7cf32f81f5eb064f66
root <root@ip-172-31-6-78.ap-northeast-2.compute.internal>
2021-05-06 14:09:42 +0000

    REGISTER OK

--- register.php
+++ register.php
@@ -1,12 +1,11 @@
 <?php
+	error_reporting(E_ALL); 
+	ini_set('display_errors', 1); 
 
-    error_reporting(E_ALL); 
-    ini_set('display_errors', 1); 
-
-    include('dbcon.php');
+	include('dbcon.php');
 
 
-    $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+	$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
 	if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $android )
 	{
@@ -17,10 +16,10 @@
 	        
 		//error check
 		if(empty($userID)){
-            $errMSG = "아이디를 입력하세요.";
-        }else if(empty($userPwd)){
-            $errMSG = "비밀번호를 입력하세요.";
-        }else if(empty($name)){
+			$errMSG = "아이디를 입력하세요.";
+		}else if(empty($userPwd)){
+			$errMSG = "비밀번호를 입력하세요.";
+		}else if(empty($name)){
 			$errMSG = "이름을 입력하세요.";
 		}else if(empty($nickname)){
 			$errMSG = "별명을 입력하세요.";
@@ -28,30 +27,30 @@
 
 		if(!isset($errMSG)){
 			try {
-				$stmt = $con->(prepare('SELECT userID, count(userID) FROM users GROUP BY userID HAVING COUNT(userID)>1'));
-				$stmt->execute();
-				$dup = $stmt->fetch(PDO::FETCH_ASSOC);
-
-				$response = array();
-
-				if($dup['name']) {
-					$responcse["success"] = false;
-				} else{
-                	$stmt = $con->prepare('INSERT INTO users VALUES(NULL, :userID, :userPwd, :name, :nickname, 0, SYSDATE())');
-                	$stmt->bindParam(':userID', $userID);
-                	$stmt->bindParam(':userPwd', $userPwd);
+				$stmts = $con->prepare('SELECT * FROM users WHERE userID = :userID');
+				$stmts->bindParam(':userID', $userID);
+				$stmts->execute();
+				$dupresult = $stmts->fetch(PDO::FETCH_ASSOC); 
+				
+				if($dupresult['name']) {
+					$errMSG = "중복 아이디입니다.";
+				} else {
+                			$stmt = $con->prepare('INSERT INTO users VALUES(NULL, :userID, :userPwd, :name, :nickname, 0, SYSDATE())');
+                			$stmt->bindParam(':userID', $userID);
+                			$stmt->bindParam(':userPwd', $userPwd);
 					$stmt->bindParam(':name', $name);
 					$stmt->bindParam(':nickname', $nickname);
 
-                	//query 실행
-                	$stmt->execute();
-                	$result = $stmt->fetch(PDO::FETCH_ASSOC); 
-                	$response["success"] = true;
-                }
-                echo json_encode($response);
-            } catch(PDOException $e) {
-                die("Database error: " . $e->getMessage()); 
-            }
+                			//query 실행
+                			if($stmt->execute()){
+						$successMSG = "success";
+					}else{ $errMSG = "fail"; }
+                			
+				}
+                		
+            		} catch(PDOException $e) {
+                		die("Database error: " . $e->getMessage()); 
+            		}
 		}
 	}
 ?>
