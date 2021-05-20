4ca58440e1a1122fbececb27dabe55c94df4aac9
soe-un <48954551+soe-un@users.noreply.github.com>
2021-05-21 03:36:58 +0900

    PLAN DATABASE OKOKOKOKOK

--- planUpdating.php
+++ planUpdating.php
@@ -104,20 +104,5 @@
 	$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
    
     if( !$android )
-    {
-?>
-    <html>
-       <body>
-
-            <form action="<?php $_PHP_SELF ?>" method="POST">
-                ID: <input type = "text" name = "ID" />
-                PASSWORD: <input type = "text" name = "PASSWORD" />
-                <input type = "submit" name = "submit" />
-            </form>
-       
-       </body>
-    </html>
-
-<?php 
-    }
+    {    }
 ?>
